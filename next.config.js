cb15bef0178508c4eff0f8b968bb42005940340a
Sebastian Arias <sarias@stackbuilders.com>
2023-03-10 14:51:32 -0500

    Add OpenAI package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    OAI_TOKEN: process.env.OAI_TOKEN,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
